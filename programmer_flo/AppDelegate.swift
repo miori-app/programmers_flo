4e85fd341d7c8c2b011f75c9b1f18fdbb4f42bab
iammiori <mioriii@naver.com>
2020-10-21 13:59:57 +0900

    [feat] splash screen 2초 delay + 음악재생화면 구상

--- programmer_flo/AppDelegate.swift
+++ programmer_flo/AppDelegate.swift
@@ -14,6 +14,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        // splash screen 2초 delay
+        sleep(2)
         return true
     }
 
